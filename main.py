6227e804c5b02d08514d861714f1489042a5a0ff
SuncheolKim <tinymin84@gmail.com>
2018-04-09 00:39:45 +0900

    for test

---
+++ main.py
@@ -0,0 +1,11 @@
+from workalendar.asia import SouthKorea
+
+c = SouthKorea
+
+print(c.FIXED_HOLIDAYS)
+print(c.chinese_new_year_eve_label)
+print(c.chinese_new_year_label)
+print(c.chinese_second_day_label)
+print(c.chinese_third_day_label)
+print(c.EASTER_METHOD)
+print(c.get_calendar_holidays(2018))
